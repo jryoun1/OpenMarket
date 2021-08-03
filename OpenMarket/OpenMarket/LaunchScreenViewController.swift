e99a8e8515adfbd719fa84b748e0388d15e1df8f
yeon <jryoun0404@gmail.com>
2021-08-04 00:56:57 +0900

    Change ViewController name to LaunchScreenViewController

---
+++ OpenMarket/OpenMarket/LaunchScreenViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LaunchScreenViewController.swift
+//  OpenMarket
+//
+//  Created by Yeon on 2021/08/03.
+//
+
+import UIKit
+
+final class LaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
