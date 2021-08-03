7b9185d82ecdbc7ce09c30cb12c68aa6fd6de6ad
yeon <jryoun0404@gmail.com>
2021-08-04 01:02:47 +0900

    feat: Add configureView method

--- OpenMarket/OpenMarket/LaunchScreenViewController.swift
+++ OpenMarket/OpenMarket/LaunchScreenViewController.swift
@@ -26,5 +26,24 @@ final class LaunchScreenViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureView()
+    }
+    
+    private func configureView() {
+        view.addSubview(logoImageView)
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+        
+        NSLayoutConstraint.activate([
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            logoImageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.2),
+            logoImageView.heightAnchor.constraint(equalTo: logoImageView.widthAnchor),
+            
+            activityIndicator.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 25),
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.widthAnchor.constraint(equalTo: logoImageView.widthAnchor, multiplier: 0.2),
+            activityIndicator.heightAnchor.constraint(equalTo: activityIndicator.widthAnchor)
+        ])
     }
 }
