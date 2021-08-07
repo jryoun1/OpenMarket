44812773097453e70b7098d1057b28ee7af9caa5
yeon <jryoun0404@gmail.com>
2021-08-07 15:39:19 +0900

    feat: Add createHTTPBodyWithData method in PatchItemAPIRequest struct

--- OpenMarket/OpenMarket/PatchItemAPIRequest.swift
+++ OpenMarket/OpenMarket/PatchItemAPIRequest.swift
@@ -9,4 +9,16 @@ import Foundation
 
 struct PatchItemAPIRequest: APIRequest {
     private let boundary: String = UUID().uuidString
+    
+    private func createHttpBodyWithData(key: String, value: [Data]) throws -> Data {
+        var body = Data()
+        for image in value {
+            body.append("--\(boundary)\r\n")
+            body.append("Content-Disposition: form-data; name=\"\(key)[]\"; filename=\"\(Date()).jpeg\"\r\n")
+            body.append("Content-Type: image/jpeg\r\n\r\n")
+            body.append(image)
+            body.append("\r\n")
+        }
+        return body
+    }
 }
