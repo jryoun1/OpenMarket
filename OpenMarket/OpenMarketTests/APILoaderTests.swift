b4bce97973a3c8ea6f80e7b715e580d470b56244
yeon <jryoun0404@gmail.com>
2021-08-07 17:00:24 +0900

    feat: Create MockURLProtocol class

--- OpenMarket/OpenMarketTests/APILoaderTests.swift
+++ OpenMarket/OpenMarketTests/APILoaderTests.swift
@@ -29,5 +29,34 @@ final class APILoaderTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+}
 
+final class MockURLProtocol: URLProtocol {
+    static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse, Data))?
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        guard let handler = MockURLProtocol.requestHandler else {
+            XCTFail("Received unexpected reqeust with no handler set")
+            return
+        }
+        
+        do {
+            let (response, data) = try handler(request)
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            client?.urlProtocol(self, didLoad: data)
+            client?.urlProtocolDidFinishLoading(self)
+        } catch {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+    }
+    
+    override func stopLoading() { }
 }
