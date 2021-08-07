f676512362b8f7402f43fb51733cba5c6d24c2ff
yeon <jryoun0404@gmail.com>
2021-08-07 12:42:49 +0900

    feat: Add loadAPIRequest method

--- OpenMarket/OpenMarket/APIRequestLoader.swift
+++ OpenMarket/OpenMarket/APIRequestLoader.swift
@@ -23,4 +23,34 @@ final class APIRequestLoader<T: APIRequest> {
         self.apiRequest = apiReqeust
         self.urlSession = urlSession
     }
+    
+    func loadAPIReqeust(requestData: T.RequestDataType,
+                        completion: @escaping (T.ResponseDataType?, Error?) -> Void) {
+        do {
+            let urlRequest = try apiRequest.makeRequest(from: requestData)
+            urlSession.dataTask(with: urlRequest) { data, response, error in
+                if let _ = error {
+                    return completion(nil, error)
+                }
+                
+                guard let httpResponse = response as? HTTPURLResponse,
+                      (200...299).contains(httpResponse.statusCode) else {
+                    return completion(nil, error)
+                }
+                
+                guard let data = data else {
+                    return completion(nil, error)
+                }
+                
+                do {
+                    let parseResponse = try self.apiRequest.parseResponse(data: data)
+                    completion(parseResponse, nil)
+                } catch {
+                    completion(nil, error)
+                }
+            }.resume()
+        } catch {
+            return completion(nil, error)
+        }
+    }
 }
