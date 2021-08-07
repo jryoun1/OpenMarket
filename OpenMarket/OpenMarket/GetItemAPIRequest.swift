565bd0f5908262c89e6bf19c5e28e0bd4b3cd32b
yeon <jryoun0404@gmail.com>
2021-08-07 13:02:43 +0900

    feat: Implement makeRequest method in GetItemAPIRequest struct

--- OpenMarket/OpenMarket/GetItemAPIRequest.swift
+++ OpenMarket/OpenMarket/GetItemAPIRequest.swift
@@ -8,4 +8,12 @@
 import Foundation
 
 struct GetItemAPIRequest: APIRequest {
+    func makeRequest(from id: Int) throws -> URLRequest {
+        guard var components = URLComponents(string: OpenMarketAPI.baseURL) else {
+            throw // error
+        }
+        components.path += "item/\(id)"
+        
+        return URLRequest(url: components.url!)
+    }
 }
