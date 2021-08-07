a553c3e74bfcae1615e3720be2a08f889c5fe0f5
yeon <jryoun0404@gmail.com>
2021-08-07 12:32:46 +0900

    feat: Create APIRequest protocol

--- OpenMarket/OpenMarket/APIRequestLoader.swift
+++ OpenMarket/OpenMarket/APIRequestLoader.swift
@@ -4,3 +4,13 @@
 //
 //  Created by Yeon on 2021/08/04.
 //
+
+import Foundation
+
+protocol APIRequest {
+    associatedtype RequestDataType
+    associatedtype ResponseDataType
+    
+    func makeRequest(from data: RequestDataType) throws -> URLRequest
+    func parseResponse(data: Data) throws -> ResponseDataType
+}
