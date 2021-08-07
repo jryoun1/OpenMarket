f2176dfbff8589e226eeed14eb1304abf068b479
yeon <jryoun0404@gmail.com>
2021-08-07 13:54:59 +0900

    feat: Add append method in Data type

--- OpenMarket/OpenMarket/Data+Append.swift
+++ OpenMarket/OpenMarket/Data+Append.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+extension Data {
+    mutating func append(_ string: String, using encoding: String.Encoding = .utf8) {
+        if let data = string.data(using: encoding) {
+            append(data)
+        }
+    }
+}
