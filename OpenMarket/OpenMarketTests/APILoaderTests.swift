b9155d2c146c6ae5eae3b4a327b925d3fe04867e
yeon <jryoun0404@gmail.com>
2021-08-07 17:02:38 +0900

    test: Add testLoaderSuccess method

--- OpenMarket/OpenMarketTests/APILoaderTests.swift
+++ OpenMarket/OpenMarketTests/APILoaderTests.swift
@@ -20,6 +20,37 @@ final class APILoaderTests: XCTestCase {
         loader = APIRequestLoader(apiReqeust: request, urlSession: urlSession)
     }
     
+    func testLoaderSuccess() {
+        let inputID = 3
+        let mockJSONData = """
+        {
+            "id": 3,
+            "title": "Mac mini",
+            "price": 890000,
+            "currency": "KRW",
+            "stock": 90,
+            "discounted_price": 89000,
+            "thumbnails": [
+                "https://camp-open-market.s3.ap-northeast-2.amazonaws.com/thumbnails/3-1.png"
+            ],
+            "registration_date": 1611523563.7245178
+        }
+        """.data(using: .utf8)!
+        MockURLProtocol.requestHandler = { request in
+            XCTAssertEqual(request.url?.path.contains("3"), true)
+            return (HTTPURLResponse(), mockJSONData)
+        }
+        
+        let expectation = XCTestExpectation(description: "response")
+        loader.loadAPIReqeust(requestData: inputID) { item, error in
+            XCTAssertEqual(item?.id, 3)
+            XCTAssertEqual(item?.title, "Mac mini")
+            XCTAssertEqual(item?.discountedPrice, 89000)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
+    }
+    
     override func tearDownWithError() throws {
         loader = nil
     }
