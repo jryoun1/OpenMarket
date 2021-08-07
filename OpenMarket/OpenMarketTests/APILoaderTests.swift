d0192d56bd5cb4a7664a4f19621c38a2b586f9f3
yeon <jryoun0404@gmail.com>
2021-08-07 17:01:15 +0900

    test: Implement setUpWithError method

--- OpenMarket/OpenMarketTests/APILoaderTests.swift
+++ OpenMarket/OpenMarketTests/APILoaderTests.swift
@@ -9,25 +9,15 @@ import XCTest
 @testable import OpenMarket
 
 final class APILoaderTests: XCTestCase {
+    var loader: APIRequestLoader<GetItemAPIRequest>!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        let request = GetItemAPIRequest()
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [MockURLProtocol.self]
+        let urlSession = URLSession(configuration: configuration)
+        
+        loader = APIRequestLoader(apiReqeust: request, urlSession: urlSession)
     }
 }
 
