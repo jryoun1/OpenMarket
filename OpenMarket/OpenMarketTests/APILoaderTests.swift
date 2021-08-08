c68352eb788f61502433589ba7c97cbc1e91dc06
yeon <jryoun0404@gmail.com>
2021-08-08 11:41:06 +0900

    test: Add testPostLoaderSuccess method

--- OpenMarket/OpenMarketTests/APILoaderTests.swift
+++ OpenMarket/OpenMarketTests/APILoaderTests.swift
@@ -10,14 +10,17 @@ import XCTest
 
 final class APILoaderTests: XCTestCase {
     var loader: APIRequestLoader<GetItemAPIRequest>!
+    var postLoader: APIRequestLoader<PostItemAPIReqeust>!
     
     override func setUpWithError() throws {
         let request = GetItemAPIRequest()
+        let postRequest = PostItemAPIReqeust()
         let configuration = URLSessionConfiguration.ephemeral
         configuration.protocolClasses = [MockURLProtocol.self]
         let urlSession = URLSession(configuration: configuration)
         
         loader = APIRequestLoader(apiReqeust: request, urlSession: urlSession)
+        postLoader = APIRequestLoader(apiReqeust: postRequest, urlSession: urlSession)
     }
     
     func testLoaderSuccess() {
@@ -61,6 +64,62 @@ final class APILoaderTests: XCTestCase {
             expectation.fulfill()
         }
         wait(for: [expectation], timeout: 5)
+    }
+    
+    func testPostLoaderSuccess() {
+        guard let airPodMaxImage1 = UIImage(named: "AirPodMax1"),
+              let airPodMaxImage2 = UIImage(named: "AirPodMax2") else {
+            return
+        }
+        
+        guard let airPodMaxImageData1 = UIImageToDataType(image: airPodMaxImage1),
+              let airPodMaxImageData2 = UIImageToDataType(image: airPodMaxImage2) else {
+            return
+        }
+        
+        var imageData: [Data] = []
+        imageData.append(airPodMaxImageData1)
+        imageData.append(airPodMaxImageData2)
+        
+        let mockJSONData = """
+        {
+            "id": 5,
+            "descriptions": "Mackbook pro 2019 16인치",
+            "title": "Macbook",
+            "price": 3650000,
+            "currency": "KRW",
+            "stock": 1000,
+            "discounted_price": 3300000,
+            "thumbnails": [
+                "https://camp-open-market.s3.ap-northeast-2.amazonaws.com/thumbnails/macbookproData.png"
+            ],
+            "registration_date": 1611523563.7245178
+        }
+        """.data(using: .utf8)!
+        
+        let postItem = ItemToUpload(title: "Macbook",
+                                    descriptions: "Mackbook pro 2019 16인치",
+                                    price: 3650000,
+                                    currency: "KRW",
+                                    stock: 1000,
+                                    discountedPrice: 3300000,
+                                    images: imageData,
+                                    password: "asdf1234")
+        
+        MockURLProtocol.requestHandler = { request in
+            XCTAssertEqual(request.url?.path.contains("item"), true)
+            return (HTTPURLResponse(), mockJSONData)
+        }
+        
+        let expectation = XCTestExpectation(description: "response")
+        postLoader.loadAPIReqeust(requestData: postItem) { item, error in
+            XCTAssertEqual(item?.id, 5)
+            XCTAssertEqual(item?.descriptions, "Mackbook pro 2019 16인치")
+            XCTAssertEqual(item?.title, "Macbook")
+            XCTAssertEqual(item?.discountedPrice, 3300000)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
     }
     
     private func UIImageToDataType(image: UIImage) -> Data? {
