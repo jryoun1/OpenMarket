c5e1e28864699e21cc5ade26c4592e3dae56ca23
yeon <jryoun0404@gmail.com>
2021-08-09 19:31:28 +0900

    Create ItemTableViewCell and ItemTableViewCell.xib file

---
+++ OpenMarket/OpenMarket/ItemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ItemTableViewCell.swift
+//  OpenMarket
+//
+//  Created by Yeon on 2021/08/09.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
