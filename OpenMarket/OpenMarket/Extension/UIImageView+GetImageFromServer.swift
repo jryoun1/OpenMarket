259701c20e6dc908ffd8dde9b171ca706460bb7c
yeon <jryoun0404@gmail.com>
2021-08-12 17:54:44 +0900

    feat: Add getImageFromServer method

--- OpenMarket/OpenMarket/Extension/UIImageView+GetImageFromServer.swift
+++ OpenMarket/OpenMarket/Extension/UIImageView+GetImageFromServer.swift
@@ -5,4 +5,19 @@
 //  Created by Yeon on 2021/08/12.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+    func getImageFromServer(_ imageURL: String) {
+        let getImageAPIReqeust = GetImageAPIRequest()
+        let apiReqeustLoader = APIRequestLoader(apiReqeust: getImageAPIReqeust)
+        
+        apiReqeustLoader.loadAPIReqeust(requestData: imageURL, completion: { image, error in
+            if let image = image {
+                DispatchQueue.main.async {
+                    self.image = image
+                }
+            }
+        })
+    }
+}
