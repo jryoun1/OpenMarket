390b5c3df9d4671e1b852e81a3704c96c122a9d2
yeon <jryoun0404@gmail.com>
2021-08-12 16:21:32 +0900

    feat: Add numberOfSections property

--- OpenMarket/OpenMarket/ItemTableViewViewModel.swift
+++ OpenMarket/OpenMarket/ItemTableViewViewModel.swift
@@ -10,3 +10,10 @@ import UIKit
 struct ItemTableViewViewModel {
     var itemList: Observable<[ItemTableViewCellViewModel]> = Observable([])
 }
+
+//MARK:- TableView Configuration Property
+extension ItemTableViewViewModel {
+    var numberOfSections: Int {
+        return 1
+    }
+}
