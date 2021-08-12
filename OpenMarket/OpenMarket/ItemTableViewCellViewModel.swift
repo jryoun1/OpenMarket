0fb304d72ab2c0bb1c75918091eb3a37d2a1b0ee
yeon <jryoun0404@gmail.com>
2021-08-12 17:26:53 +0900

    feat: Add price property

--- OpenMarket/OpenMarket/ItemTableViewCellViewModel.swift
+++ OpenMarket/OpenMarket/ItemTableViewCellViewModel.swift
@@ -9,4 +9,11 @@ import Foundation
 
 struct ItemTableViewCellViewModel {
     private let item: Item
+    
+    var price: String {
+        if let price = CustomNumberFormatter.commaFormatter.string(from: NSNumber(value: self.item.price)) {
+            return "\(self.item.currency) \(price)"
+        }
+        return "가격을 표시하는데 문제가 발생했습니다."
+    }
 }
