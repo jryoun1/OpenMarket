7e1df0ba8c8921ec1fc7da4a18efc82cc20cc1bb
yeon <jryoun0404@gmail.com>
2021-08-16 00:55:57 +0900

    feat: Add configureSegmentControl method

--- OpenMarket/OpenMarket/HomeViewController.swift
+++ OpenMarket/OpenMarket/HomeViewController.swift
@@ -19,4 +19,26 @@ final class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    //MARK:- SegmentControl
+    private func configureSegmentControl() {
+        let titles = ["LIST", "GRID"]
+        let segmentedControl: UISegmentedControl = UISegmentedControl(items: titles)
+        segmentedControl.selectedSegmentTintColor = UIColor.systemBlue
+        segmentedControl.backgroundColor = UIColor.white
+        segmentedControl.selectedSegmentIndex = 0
+        for index in 0..<titles.count {
+            segmentedControl.setWidth(80, forSegmentAt: index)
+        }
+        segmentedControl.layer.borderWidth = 1.0
+        segmentedControl.layer.borderColor = UIColor.systemBlue.cgColor
+        segmentedControl.layer.masksToBounds = true
+        segmentedControl.sizeToFit()
+        segmentedControl.setTitleTextAttributes([.foregroundColor: UIColor.systemBlue], for: .normal)
+        segmentedControl.setTitleTextAttributes([.foregroundColor: UIColor.white], for: .selected)
+        
+        segmentedControl.addTarget(self, action: #selector(segmentChanged), for: .valueChanged)
+        segmentedControl.sendActions(for: .valueChanged)
+        navigationItem.titleView = segmentedControl
+    }
 }
