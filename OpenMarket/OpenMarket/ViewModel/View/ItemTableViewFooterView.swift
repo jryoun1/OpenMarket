42b314b27bce2e05440df82dd469ae3c4b73e14e
yeon <jryoun0404@gmail.com>
2021-08-18 00:37:53 +0900

    feat: Add loadingIndicator property

--- OpenMarket/OpenMarket/ViewModel/View/ItemTableViewFooterView.swift
+++ OpenMarket/OpenMarket/ViewModel/View/ItemTableViewFooterView.swift
@@ -9,4 +9,11 @@ import UIKit
 
 final class ItemTableViewFooterView: UITableViewHeaderFooterView {
     static let identifier = "ItemTableViewFooterView"
+    private var loadingIndicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView()
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        indicator.color = .systemGray
+        indicator.style = .medium
+        return indicator
+    }()
 }
