cc4b1a72219783900b5a160ece3d77a461b2727b
yeon <jryoun0404@gmail.com>
2021-08-18 00:36:32 +0900

    feat: Add identifier property

--- OpenMarket/OpenMarket/ViewModel/View/ItemTableViewFooterView.swift
+++ OpenMarket/OpenMarket/ViewModel/View/ItemTableViewFooterView.swift
@@ -7,14 +7,6 @@
 
 import UIKit
 
-class ItemTableViewFooterView: UITableViewHeaderFooterView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+final class ItemTableViewFooterView: UITableViewHeaderFooterView {
+    static let identifier = "ItemTableViewFooterView"
 }
