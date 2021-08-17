21b9c2e166da8b657da05d47c936f833fe7ff1d1
yeon <jryoun0404@gmail.com>
2021-08-18 00:48:33 +0900

    feat: Add IBOutlet properties

--- OpenMarket/OpenMarket/ViewModel/View/ItemCollectionViewCell.swift
+++ OpenMarket/OpenMarket/ViewModel/View/ItemCollectionViewCell.swift
@@ -9,4 +9,11 @@ import UIKit
 
 final class ItemCollectionViewCell: UICollectionViewCell {
     static let identifier = "ItemCollectionViewCell"
+    @IBOutlet private var itemImageView: UIImageView!
+    @IBOutlet private var titleLabel: UILabel!
+    @IBOutlet private var stockLabel: UILabel!
+    @IBOutlet private var priceLabel: UILabel!
+    @IBOutlet private var discountedPriceLabel: UILabel!
+    @IBOutlet private var verticalStackView: UIStackView!
+    
 }
