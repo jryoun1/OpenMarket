55ce49078fe49dbb880a8c52d67a4b5412279c4b
yeon <jryoun0404@gmail.com>
2021-08-18 00:46:22 +0900

    Create ItemCollectionViewCell file and xib file

---
+++ OpenMarket/OpenMarket/ViewModel/View/ItemCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCollectionViewCell.swift
+//  OpenMarket
+//
+//  Created by Yeon on 2021/08/18.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
