51741a842a4071bd62f81e0736fbc3908f399d3e
yeon <jryoun0404@gmail.com>
2021-08-18 00:35:00 +0900

    Create ItemTableViewFooterView file

---
+++ OpenMarket/OpenMarket/ViewModel/View/ItemTableViewFooterView.swift
@@ -0,0 +1,20 @@
+//
+//  ItemTableViewFooterView.swift
+//  OpenMarket
+//
+//  Created by Yeon on 2021/08/17.
+//
+
+import UIKit
+
+class ItemTableViewFooterView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
