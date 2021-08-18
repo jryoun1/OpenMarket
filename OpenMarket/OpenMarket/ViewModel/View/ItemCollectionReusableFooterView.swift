177567860b3e5d1fa60ecdde91cc901795e18955
yeon <jryoun0404@gmail.com>
2021-08-18 22:44:21 +0900

    Create ItemCollectionReusableFooterView file

---
+++ OpenMarket/OpenMarket/ViewModel/View/ItemCollectionReusableFooterView.swift
@@ -0,0 +1,12 @@
+//
+//  ItemCollectionReusableFooterView.swift
+//  OpenMarket
+//
+//  Created by Yeon on 2021/08/18.
+//
+
+import UIKit
+
+class ItemCollectionReusableFooterView: UICollectionReusableView {
+        
+}
