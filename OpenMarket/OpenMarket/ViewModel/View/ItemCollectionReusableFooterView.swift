1094c14c67c32de68c361c062e7700c77a89b98e
yeon <jryoun0404@gmail.com>
2021-08-19 00:32:32 +0900

    feat: Add loadingIndicator property

--- OpenMarket/OpenMarket/ViewModel/View/ItemCollectionReusableFooterView.swift
+++ OpenMarket/OpenMarket/ViewModel/View/ItemCollectionReusableFooterView.swift
@@ -9,4 +9,11 @@ import UIKit
 
 final class ItemCollectionReusableFooterView: UICollectionReusableView {
     static let identifier = "ItemCollectionReusableFooterView"
+    private var loadingIndicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView()
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        indicator.color = .systemGray
+        indicator.style = .medium
+        return indicator
+    }()
 }
