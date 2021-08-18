53e98e8d46b5a7af825a7c6c582d357f658cbe0b
yeon <jryoun0404@gmail.com>
2021-08-19 00:56:17 +0900

    feat: Add collectionView minimumLineSpacingForSectionAt method

--- OpenMarket/OpenMarket/HomeViewController.swift
+++ OpenMarket/OpenMarket/HomeViewController.swift
@@ -180,4 +180,8 @@ extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSour
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
 }
