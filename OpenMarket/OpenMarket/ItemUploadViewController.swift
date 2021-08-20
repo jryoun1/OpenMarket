437b5b3ab57ca7cc9f5c4315ae68f51185ec7e05
yeon <jryoun0404@gmail.com>
2021-08-20 18:11:27 +0900

    feat: Add identifier property

--- OpenMarket/OpenMarket/ItemUploadViewController.swift
+++ OpenMarket/OpenMarket/ItemUploadViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 final class ItemUploadViewController: UIViewController {
-
+    static let identifier = "ItemUploadViewController"
     override func viewDidLoad() {
         super.viewDidLoad()
     }
