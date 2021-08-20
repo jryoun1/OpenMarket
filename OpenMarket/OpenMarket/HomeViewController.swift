14eef402ab659b922216d2f48a73f21e7e41f0a7
yeon <jryoun0404@gmail.com>
2021-08-21 00:07:28 +0900

    feat: Add tableView didSelectRowAt method

--- OpenMarket/OpenMarket/HomeViewController.swift
+++ OpenMarket/OpenMarket/HomeViewController.swift
@@ -257,6 +257,10 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
 }
 
 //MARK:- CollectionView Delegate, DataSource, DelegateFlowLayout
