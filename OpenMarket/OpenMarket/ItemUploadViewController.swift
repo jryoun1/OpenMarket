f1992f00cacfb298c043ed294d58addf4c6a6a57
yeon <jryoun0404@gmail.com>
2021-08-25 22:07:57 +0900

    feat: Add IBOutlet properties and connect with InterfaceBuilder

--- OpenMarket/OpenMarket/ItemUploadViewController.swift
+++ OpenMarket/OpenMarket/ItemUploadViewController.swift
@@ -8,6 +8,17 @@
 import UIKit
 
 final class ItemUploadViewController: UIViewController {
+    @IBOutlet weak var imageCollectionView: UICollectionView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var verticalStackView: UIStackView!
+    @IBOutlet weak var titleTextView: UITextView!
+    @IBOutlet weak var stockTextView: UITextView!
+    @IBOutlet weak var priceTextView: UITextView!
+    @IBOutlet weak var discountedPriceTextView: UITextView!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var passwordCheckTextfield: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
     static let identifier = "ItemUploadViewController"
     override func viewDidLoad() {
         super.viewDidLoad()
