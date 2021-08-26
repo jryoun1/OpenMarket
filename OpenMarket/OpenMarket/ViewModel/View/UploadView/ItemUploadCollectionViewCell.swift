7a296fb37637554e5a38d7f9a8bd852de1a374a7
yeon <jryoun0404@gmail.com>
2021-08-26 20:45:23 +0900

    feat: Add DeleteImage protocol

--- OpenMarket/OpenMarket/ViewModel/View/UploadView/ItemUploadCollectionViewCell.swift
+++ OpenMarket/OpenMarket/ViewModel/View/UploadView/ItemUploadCollectionViewCell.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
+protocol DeleteImage: AnyObject {
+    func delete(index: Int)
+}
+
 final class ItemUploadCollectionViewCell: UICollectionViewCell {
     static let identifier = "ItemUploadCollectionViewCell"
     @IBOutlet private weak var itemImageView: UIImageView!
     @IBOutlet private weak var deleteButton: UIButton!
+    weak var deleteImageDelegate: DeleteImage?
     
     override func awakeFromNib() {
         super.awakeFromNib()
+    }
+    
+    @IBAction private func didDeleteButtonTouchedUp(_ sender: UIButton) {
+        deleteImageDelegate?.delete(index: self.tag)
     }
     
     func configure(image: UIImage) {
