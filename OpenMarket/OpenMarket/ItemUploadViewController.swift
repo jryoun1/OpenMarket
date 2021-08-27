2dbe4efef92b62981af371c5c290e118d6a7d22f
yeon <jryoun0404@gmail.com>
2021-08-27 23:40:50 +0900

    fix: Change ItemUploadViewController view and IBOutlet properties

--- OpenMarket/OpenMarket/ItemUploadViewController.swift
+++ OpenMarket/OpenMarket/ItemUploadViewController.swift
@@ -9,12 +9,11 @@ import UIKit
 
 final class ItemUploadViewController: UIViewController {
     @IBOutlet weak var imageCollectionView: UICollectionView!
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var verticalStackView: UIStackView!
-    @IBOutlet weak var titleTextView: UITextView!
-    @IBOutlet weak var stockTextView: UITextView!
-    @IBOutlet weak var priceTextView: UITextView!
-    @IBOutlet weak var discountedPriceTextView: UITextView!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var currencyTextField: UITextField!
+    @IBOutlet weak var stockTextField: UITextField!
+    @IBOutlet weak var priceTextField: UITextField!
+    @IBOutlet weak var discountedPriceTextField: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     @IBOutlet weak var passwordCheckTextfield: UITextField!
     @IBOutlet weak var descriptionTextView: UITextView!
