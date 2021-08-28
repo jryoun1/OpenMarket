7e646b1616557cc3dc167b385f55752870b8605e
yeon <jryoun0404@gmail.com>
2021-08-28 13:50:32 +0900

    feat: Add ItemUploadViewString

--- OpenMarket/OpenMarket/Utilities/OpenMarketViewString.swift
+++ OpenMarket/OpenMarket/Utilities/OpenMarketViewString.swift
@@ -14,4 +14,12 @@ enum ItemListViewString {
 enum ItemUploadViewString {
     static let doneButton = "완료"
     static let selectButton = "선택"
+    static let imageCountLimitMessage = "이미지를 최소 1개 이상 5개 이하로 선택해주세요."
+    static let titleEmptyMessage = "제품명을 입력해주세요"
+    static let currencyEmptyMessage = "화폐를 선택해주세요"
+    static let priceEmptyMessage = "가격을 입력해주세요"
+    static let stockEmptyMessage = "수량을 입력해주세요"
+    static let passwordEmptyMessage = "비밀번호를 입력해주세요"
+    static let descriptionEmptyMessage = "상세 설명을 입력해주세요"
+    static let descriptionPlaceholder = "게시글 내용을 작성해주세요. (가품 및 판매금지품목은 게시가 제한될 수 있어요.)"
 }
