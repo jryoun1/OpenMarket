7387cf32963c58e3a9f454f6297dd186234b7ab2
yeon <jryoun0404@gmail.com>
2021-08-28 14:39:57 +0900

    feat: Add Observable properties

--- OpenMarket/OpenMarket/ViewModel/ItemUploadViewModel.swift
+++ OpenMarket/OpenMarket/ViewModel/ItemUploadViewModel.swift
@@ -9,6 +9,15 @@ import UIKit
 
 final class ItemUploadViewModel {
     var selectedImageData: Observable<[Data]> = Observable([])
+    var itemToUploadsInputErrorMessage: Observable<String> = Observable("")
+    var isTitleTextFieldHighLighted: Observable<Bool> = Observable(false)
+    var isCurrencyTextFieldHighLighted: Observable<Bool> = Observable(false)
+    var isPriceTextFieldHighLighted: Observable<Bool> = Observable(false)
+    var isDiscountedPriceTextFieldHighLighted: Observable<Bool> = Observable(false)
+    var isStockTextFieldHighLighted: Observable<Bool> = Observable(false)
+    var isPasswordTextFieldHighLighted: Observable<Bool> = Observable(false)
+    var isDescriptionTextViewHighLighted: Observable<Bool> = Observable(false)
+    var networkErrorMessage: Observable<String?> = Observable(nil)
 }
 
 //MARK:- TableView Configuration Property
