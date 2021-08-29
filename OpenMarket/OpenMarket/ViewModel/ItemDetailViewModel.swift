d4d24752277b5c0194a24b440830eb097cb68deb
yeon <jryoun0404@gmail.com>
2021-08-30 01:46:04 +0900

    feat: Add fetchItem method

--- OpenMarket/OpenMarket/ViewModel/ItemDetailViewModel.swift
+++ OpenMarket/OpenMarket/ViewModel/ItemDetailViewModel.swift
@@ -20,4 +20,45 @@ final class ItemDetailViewModel {
     init(id: Int) {
         self.id = id
     }
+    
+    func fetchItem() {
+        let getItemAPIRequest = GetItemAPIRequest()
+        let apiRequestLoader = APIRequestLoader(apiReqeust: getItemAPIRequest)
+        
+        apiRequestLoader.loadAPIReqeust(requestData: id) { [weak self] item, error in
+            if let error = error {
+                self?.errorText.value = error.localizedDescription
+            }
+            
+            guard let item = item else {
+                return
+            }
+            
+            self?.titleLabeltext.value = item.title
+            
+            let priceWithComma = CustomNumberFormatter.commaFormatter.string(from: NSNumber(value: item.price))!
+            self?.priceLabeltext.value = "\(item.currency) \(priceWithComma)"
+            
+            if let discountedPrice = item.discountedPrice {
+                let discountedPriceWithComma = CustomNumberFormatter.commaFormatter.string(from: NSNumber(value: discountedPrice))!
+                self?.discountedPriceLabeltext.value = "\(item.currency) \(discountedPriceWithComma)"
+            }
+            
+            if item.stock <= 0 {
+                self?.stockTextLabeltext.value = ItemListViewString.soldOut
+            }
+            else {
+                let stock = CustomNumberFormatter.commaFormatter.string(from: NSNumber(value: item.stock))!
+                self?.stockTextLabeltext.value = "\(ItemListViewString.remainingQuantity) \(stock)"
+            }
+            
+            self?.descriptionLabeltext.value = item.descriptions
+            
+            if let imageURLs = item.imageURLs {
+                for imageURL in imageURLs {
+                    self?.downloadImage(url: imageURL)
+                }
+            }
+        }
+    }
 }
