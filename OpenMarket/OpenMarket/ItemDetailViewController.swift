af7c187412f1cfeb94367e674009329ebc9a16b4
yeon <jryoun0404@gmail.com>
2021-08-30 01:38:40 +0900

    feat: Add IBOutlet properties

--- OpenMarket/OpenMarket/ItemDetailViewController.swift
+++ OpenMarket/OpenMarket/ItemDetailViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 final class ItemDetailViewController: UIViewController {
+    @IBOutlet weak var imageCollectionView: UICollectionView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var stockLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var discountedLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     static let identifier = "ItemDetailViewController"
     
     override func viewDidLoad() {
