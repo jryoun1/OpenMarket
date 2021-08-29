1651abe3459f897746feb7dba6970566d33a501e
yeon <jryoun0404@gmail.com>
2021-08-30 01:49:11 +0900

    feat: Add bindViewModel method

--- OpenMarket/OpenMarket/ItemDetailViewController.swift
+++ OpenMarket/OpenMarket/ItemDetailViewController.swift
@@ -20,5 +20,61 @@ final class ItemDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        bindViewModel()
+    }
+    
+    private func bindViewModel() {
+        itemDetailViewModel?.titleLabeltext.bind({ [weak self] text in
+            DispatchQueue.main.async {
+                self?.titleLabel.text = text
+            }
+        })
+        
+        itemDetailViewModel?.stockTextLabeltext.bind({ [weak self] text in
+            DispatchQueue.main.async {
+                if text == ItemListViewString.soldOut {
+                    self?.stockLabel.textColor = .systemOrange
+                }
+                else {
+                    self?.stockLabel.textColor = .systemGray
+                }
+                self?.stockLabel.text = text
+            }
+        })
+        
+        itemDetailViewModel?.priceLabeltext.bind({ [weak self] text in
+            DispatchQueue.main.async {
+                self?.priceLabel.attributedText = NSAttributedString(string: text ?? "")
+            }
+        })
+        
+        itemDetailViewModel?.discountedPriceLabeltext.bind({ [weak self] text in
+            if let text = text, !text.isEmpty {
+                DispatchQueue.main.async {
+                    self?.discountedLabel.isHidden = false
+                    self?.discountedLabel.text = text
+                    
+                    self?.priceLabel.textColor = .systemRed
+                    self?.priceLabel.attributedText = self?.changeToStrikethroughStyle(string: self?.priceLabel.text! ?? "")
+                }
+            }
+            else {
+                DispatchQueue.main.async {
+                    self?.discountedLabel.isHidden = true
+                }
+            }
+        })
+        
+        itemDetailViewModel?.descriptionLabeltext.bind({ [weak self] text in
+            DispatchQueue.main.async {
+                self?.descriptionLabel.text = text
+            }
+        })
+        
+        itemDetailViewModel?.images.bind({ [weak self] images in
+            DispatchQueue.main.async {
+                self?.imageCollectionView.reloadData()
+            }
+        })
     }
 }
