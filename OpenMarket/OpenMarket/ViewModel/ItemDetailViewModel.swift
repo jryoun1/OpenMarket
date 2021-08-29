f263923d6979be6bc3a2a550b143fc18ebd668f5
yeon <jryoun0404@gmail.com>
2021-08-30 01:44:49 +0900

    feat: Add Observable properties used in ItemListViewController

--- OpenMarket/OpenMarket/ViewModel/ItemDetailViewModel.swift
+++ OpenMarket/OpenMarket/ViewModel/ItemDetailViewModel.swift
@@ -9,7 +9,14 @@ import UIKit
 
 final class ItemDetailViewModel {
     private let id: Int
-
+    var titleLabeltext: Observable<String> = Observable("")
+    var priceLabeltext: Observable<String> = Observable("")
+    var discountedPriceLabeltext: Observable<String> = Observable("")
+    var stockTextLabeltext: Observable<String> = Observable("")
+    var descriptionLabeltext: Observable<String> = Observable("")
+    var errorText: Observable<String> = Observable("")
+    var images: Observable<[UIImage]> = Observable([])
+    
     init(id: Int) {
         self.id = id
     }
