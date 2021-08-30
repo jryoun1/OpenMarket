d29bedc2ad09ed4b4236085bd3562e4cb3b503d5
yeon <jryoun0404@gmail.com>
2021-08-31 00:21:32 +0900

    feat: Add AlertShowable protocol

--- OpenMarket/OpenMarket/Error/AlertShowable.swift
+++ OpenMarket/OpenMarket/Error/AlertShowable.swift
@@ -5,11 +5,15 @@
 //  Created by Yeon on 2021/08/30.
 //
 
-import Foundation
+import UIKit
 
 enum AlertString {
     static let normalTitle = "알림"
     static let okButtonTitle = "확인"
     static let passwordNotCorrectErrorMessage = "비밀번호가 일치하지 않아서 해당 상품을 삭제나 수정할 수 없습니다"
     static let passwordNotCorrectErrorAlertTitle = "비밀번호가 일치하지 않습니다"
+}
+
+protocol AlertShowable {
+    func showAlert(viewController: UIViewController, _ error: OpenMarketError)
 }
